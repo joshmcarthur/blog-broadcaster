2dc615450d0e4fcc59ed9a818fcde84577ee2c15
Josh McArthur <joshua.mcarthur@gmail.com>
2011-09-12 23:39:40 +1200

    Implemented Facebook broadcasting

--- lib/jekyll_broadcaster/facebook_broadcaster.rb
+++ lib/jekyll_broadcaster/facebook_broadcaster.rb
@@ -1,14 +1,15 @@
 module JekyllBroadcaster
   class FacebookBroadcaster
-    OAUTH_TOKEN = ENV['OAUTH_TOKEN']
-    OAUTH_SECRET = ENV['OAUTH_SECRET']
-  
-    CLIENT_TOKEN = ENV['CLIENT_TOKEN']
-    CLIENT_SECRET = ENV['CLIENT_SECRET']
-
     def self.do_it!(message = "I've pushed new content up to my blog!", url = "")
-      raise "I need a message and a URL" unless message && url
+      raise "I need a message, an access token, and a URL" unless message && ENV['FACEBOOK_ACCESS_TOKEN'] && url
       # Broadcast me!
+      FbGraph::User.me(ENV['FACEBOOK_ACCESS_TOKEN']).feed!(
+        :message => message,
+        :link => url,
+        :name => "Blog Broadcaster",
+        :description => "Publishes blog posts on social networks from Git pushes."
+      )
     end
   end
 end
+
