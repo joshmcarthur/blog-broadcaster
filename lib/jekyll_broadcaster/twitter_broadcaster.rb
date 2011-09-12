298e5076b586214474971d0073c9794a79c4ec39
Josh McArthur <joshua.mcarthur@gmail.com>
2011-09-12 20:28:02 +1200

    Added actual tweeting capability

--- lib/jekyll_broadcaster/twitter_broadcaster.rb
+++ lib/jekyll_broadcaster/twitter_broadcaster.rb
@@ -1,14 +1,31 @@
 module JekyllBroadcaster
   class TwitterBroadcaster
-    OAUTH_TOKEN = ENV['OAUTH_TOKEN']
-    OAUTH_SECRET = ENV['OAUTH_SECRET']
-  
-    CLIENT_TOKEN = ENV['CLIENT_TOKEN']
-    CLIENT_SECRET = ENV['CLIENT_SECRET']
+    def self.consumer_token
+      ENV['TWITTER_CONSUMER_TOKEN']
+    end
+
+    def self.consumer_secret
+      ENV['TWITTER_CONSUMER_SECRET']
+    end
+
+    def self.access_token
+      ENV['TWITTER_ACCESS_TOKEN']
+    end
+
+    def self.access_secret
+      ENV['TWITTER_ACCESS_SECRET']
+    end
 
     def self.do_it!(message = "I've pushed new content up to my blog!", url = "")
+      Twitter.configure do |config|
+        config.consumer_key = ENV['TWITTER_CONSUMER_TOKEN']
+        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+        config.oauth_token = ENV['TWITTER_ACCESS_TOKEN']
+        config.oauth_secret = ENV['TWITTER_ACCESS_SECRET']
+      end
       raise "I need a 140 char message and a URL." unless message && url && message.length < 140
-      #Broadcast me!
+      Twitter.update(message[0..120] + " " + url)
     end
   end
 end
+
